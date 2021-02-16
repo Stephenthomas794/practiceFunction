4e6b76af57bbd0aa381db30b3387412df0564e29
Stephen Thomas <stephenthomas794@gmail.com>
2021-02-16 12:39:13 -0500

    test

--- function.py
+++ function.py
@@ -1,1 +1,9 @@
-print("Hello")
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    print("hello")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
